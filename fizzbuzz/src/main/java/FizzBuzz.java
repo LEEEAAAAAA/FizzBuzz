25b6d27a7ca46e47ee7f4975dee8d56736d79217
LEEEAAAAAA <lea.schuerch@gmail.com>
2026-01-13 09:40:33 +0100

    TDD - first test

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,2 +1,6 @@
 public class FizzBuzz {
+
+    public int fizzBuzz() {
+        return 1;
+    }
 }
--- fizzbuzz/src/test/java/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -1,5 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+    FizzBuzz fizzBuzz;
 
+@BeforeEach
+void setUp() {
+    fizzBuzz = new FizzBuzz();
+}
+
+    @Test
+    void fizzBuzz() {
+        int expected = 1;
+
+        int result = fizzBuzz.fizzBuzz();
+
+        assertEquals(expected, result);
+    }
 }
